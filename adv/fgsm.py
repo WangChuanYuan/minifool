82d11e4e71a107b67a3e752d2079bd4fb74e96c6
161250135 <161250135@smail.nju.edu.cn>
2019-06-24 22:13:09 +0800

    MI-FGSM

--- adv/fgsm.py
+++ adv/fgsm.py
@@ -2,13 +2,13 @@ import numpy as np
 from keras import backend as K
 
 
-class FGSMAttacker(object):
+class MIFGSMAttacker(object):
     def __init__(self, model, imgs, dimensions=(28, 28, 1)):
         self.model = model
         self.imgs = imgs
         self.dimensions = dimensions
 
-    def attack_all(self, target=None):
+    def attack_all(self, target=None, verbose=True):
         model_input_layer = self.model.layers[0].input
         model_output_layer = self.model.layers[-1].output
 
@@ -16,7 +16,6 @@ class FGSMAttacker(object):
         idx = 0
         for image in self.imgs:
             idx += 1
-            print("Generate adversarial sample for image {}".format(idx))
             # Add a 4th dimension for batch size
             original_image = np.expand_dims(image, axis=0)
             original_image = original_image.astype('float32')
@@ -42,38 +41,47 @@ class FGSMAttacker(object):
             grab_cost_and_gradients_from_model = K.function([model_input_layer, K.learning_phase()],
                                                             [cost_function, gradient_function])
 
-            learning_rate = 0.005
+            learning_rate = 0.0005
+            momentum = 0
+            decay_factor = 1
 
             itr = 0
             finish = False
+            success = False
             while not finish:
+                itr += 1
+
                 # Check how close the image is to our target class and grab the gradients we
                 # can use to push it one more step in that direction.
                 # Note: It's really important to pass in '0' for the Keras learning mode here!
                 # Keras layers behave differently in prediction vs. train modes!
                 cost, gradients = grab_cost_and_gradients_from_model([hacked_image, 0])
 
-                # Move the hacked image one step further towards fooling the model
                 if target is None:
-                    hacked_image -= np.sign(gradients) * learning_rate
-                else:
-                    hacked_image += np.sign(gradients) * learning_rate
+                    gradients = -gradients
+
+                velocity = gradients / np.linalg.norm(np.reshape(gradients, self.dimensions[0] * self.dimensions[1]), ord=1)
+                momentum = decay_factor * momentum + velocity
+                hacked_image += np.sign(gradients) * learning_rate
 
                 # Ensure that the image doesn't ever change too much to either look funny or to become an invalid image
                 hacked_image = np.clip(hacked_image, max_change_below, max_change_above)
                 hacked_image = np.clip(hacked_image, 0.0, 1.0)
 
                 if target is None:
-                    if cost < 0.1:
+                    if itr > 1500 or cost < 0.1:
                         finish = True
+                        success = True if cost < 0.1 else False
                 else:
-                    if cost > 0.8:
+                    if itr > 1500 or cost > 0.8:
                         finish = True
+                        success = True if cost > 0.8 else False
 
-                itr += 1
-                print("Iteration: {} Cost: {:.8}%".format(itr, cost * 100))
-                if itr > 1200:
-                    break  # End while
+                if verbose:
+                    print("Iteration: {} Cost: {:.8}%".format(itr, cost * 100))
+                    if finish:
+                        print("Image{} attack success: {}".format(idx, success))
+                        print("--------------------")  # end while
 
             hacked_image = hacked_image[0]
             hacked_image *= 255
--- adv/one_pixel_attack.py
+++ adv/one_pixel_attack.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-import util.perturber as pb
 from util.image_comparator import comparator
 from util.differential_evolution import differential_evolution
 
@@ -12,17 +11,43 @@ class PixelAttacker(object):
         self.probs = model.predict(imgs / 255)
         self.dimensions = dimensions
 
+    @staticmethod
+    def perturb_image(xs, img):
+        # If this function is passed just one perturbation vector,
+        # pack it in a list to keep the computation the same
+        if xs.ndim < 2:
+            xs = np.array([xs])
+
+        # Copy the image n == len(xs) times so that we can
+        # create n new perturbed images
+        tile = [len(xs)] + [1] * (xs.ndim + 1)
+        imgs = np.tile(img, tile)
+
+        # Make sure to floor the members of xs as int types
+        xs = xs.astype(int)
+
+        for x, img in zip(xs, imgs):
+            # Split x into an array of 3-tuples (perturbation pixels)
+            # i.e., [[x,y,c], ...]
+            pixels = np.split(x, len(x) // 3)
+            for pixel in pixels:
+                # At each pixel's x,y position, assign its gray value
+                x_pos, y_pos, c = pixel
+                img[x_pos, y_pos, 0] = c
+
+        return imgs
+
     # The untargeted attack is to minimize the confidence of the model in the correct target class
     def predict_classes(self, xs, img, target_class, minimize=True):
         # Perturb the image with the given pixel(s) x and get the prediction of the model
-        imgs_perturbed = pb.perturb_image(xs, img)
+        imgs_perturbed = PixelAttacker.perturb_image(xs, img)
         predictions = self.model.predict(imgs_perturbed / 255)[:, target_class]
         # This function should always be minimized, so return its complement if needed
         return predictions if minimize else 1 - predictions
 
     def is_attack_success(self, x, img, target_class, targeted_attack=False, verbose=False):
         # Perturb the image with the given pixel(s) and get the prediction of the model
-        attack_image = pb.perturb_image(x, img)
+        attack_image = PixelAttacker.perturb_image(x, img)
 
         confidence = self.model.predict(attack_image / 255)[0]
         predicted_class = np.argmax(confidence)
@@ -58,7 +83,7 @@ class PixelAttacker(object):
             predict_fn, bounds, maxiter=maxiter, popsize=popmul,
             recombination=1, atol=-1, callback=callback_fn, polish=False)
 
-        hacked_image = pb.perturb_image(attack_result.x, original_image)[0]
+        hacked_image = PixelAttacker.perturb_image(attack_result.x, original_image)[0]
 
         if verbose:
             prior_probs = self.model.predict(np.array([original_image]) / 255)[0]
