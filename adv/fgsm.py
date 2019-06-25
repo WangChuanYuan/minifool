52c1a711e19c6ce733ba53d0ef3e43588b9e8601
161250135 <161250135@smail.nju.edu.cn>
2019-06-25 11:50:22 +0800

    MI-FGSM failed

--- adv/fgsm.py
+++ adv/fgsm.py
@@ -1,3 +1,5 @@
+from collections import Iterable
+
 import numpy as np
 from keras import backend as K
 from keras.layers import Dense
@@ -17,7 +19,14 @@ class MIFGSMAttacker(object):
         dense_layer.set_weights(self.model.layers[-1].get_weights())
         return logits_model
 
-    def attack_all(self, target=None, verbose=True):
+    def attack_all(self,
+                   epsilons=0.0005,
+                   epsilons_max=0.005,
+                   steps=100,
+                   epsilon_steps=100,
+                   decay_factor=1,
+                   target=None,
+                   verbose=True):
         logits_model = self.get_logits_model()
         model_input = logits_model.layers[0].input
         model_output = logits_model.layers[-1].output
@@ -34,65 +43,76 @@ class MIFGSMAttacker(object):
             max_change_above = original_image + 0.05
             max_change_below = original_image - 0.05
 
-            # Create a copy of the input image to hack on
             hacked_image = np.copy(original_image)
 
-            # Decide object_type_to_fake on whether is target attack
-            object_type_to_fake = np.argmax(self.model.predict(original_image)[0]) if target is None else target
+            # Decide target_class on whether is target attack
+            actual_class = np.argmax(self.model.predict(original_image)[0])
+            target_class = actual_class if target is None else target
 
-            cost_function = model_output[0, object_type_to_fake]
+            # Create a Keras function
+            cost_fn = model_output[0, target_class]
+            predict_fn = self.model.layers[-1].output[0]
+            gradient_fn = K.gradients(cost_fn, model_input)[0]
 
-            confidence_function = self.model.layers[-1].output[0, object_type_to_fake]
+            # Note: It's really important to pass in '0' for the Keras learning mode when used
+            cost_and_gradients_from_logits_model = K.function([model_input, K.learning_phase()], [cost_fn, gradient_fn])
+            predict_from_model = K.function([model_input, K.learning_phase()], [predict_fn])
 
-            gradient_function = K.gradients(cost_function, model_input)[0]
+            if not isinstance(epsilons, Iterable):
+                epsilons = np.linspace(epsilons, epsilons_max, num=epsilon_steps)
 
-            # Create a Keras function that we can call to calculate the current cost and gradient
-            grab_cost_and_gradients_from_model = K.function([model_input, K.learning_phase()],
-                                                            [cost_function, confidence_function, gradient_function])
-
-            learning_rate = 0.0005
-            momentum = 0
-            decay_factor = 1
-
-            itr = 0
             finish = False
             success = False
-            while not finish:
-                itr += 1
+            step = 0
+            for epsilon in epsilons[:]:
+                if success or finish:
+                    break
+                if epsilon == 0.0:
+                    continue
+                momentum = 0
+                for i in range(steps):
+                    step += 1
 
-                # Note: It's really important to pass in '0' for the Keras learning mode here!
-                cost, confidence, gradients = grab_cost_and_gradients_from_model([hacked_image, 0])
+                    cost, gradients = cost_and_gradients_from_logits_model([hacked_image, 0])
 
-                if target is None:
-                    gradients = -gradients
+                    if target is None:
+                        gradients = -gradients
 
-                velocity = gradients / np.linalg.norm(np.reshape(gradients, self.dimensions[0] * self.dimensions[1]), ord=1)
-                momentum = decay_factor * momentum + velocity
-                hacked_image += np.sign(gradients) * learning_rate
+                    velocity = gradients / np.linalg.norm(
+                        np.reshape(gradients, self.dimensions[0] * self.dimensions[1]), ord=1)
+                    momentum = decay_factor * momentum + velocity
+                    hacked_image += np.sign(gradients) * epsilon
 
-                # Ensure that the image doesn't ever change too much to either look funny or to become an invalid image
-                hacked_image = np.clip(hacked_image, max_change_below, max_change_above)
-                hacked_image = np.clip(hacked_image, 0.0, 1.0)
+                    # Ensure that the image doesn't change too much
+                    hacked_image = np.clip(hacked_image, max_change_below, max_change_above)
+                    hacked_image = np.clip(hacked_image, 0.0, 1.0)
 
-                if target is None:
-                    if itr > 1000 or confidence < 0.1:
+                    confidence= predict_from_model([hacked_image, 0])[0]
+                    predict_class = np.argmax(confidence)
+
+                    if verbose:
+                        print("Step{}: confidence {:.8}%".format(step, confidence[target_class] * 100))
+
+                    if step > 800:
                         finish = True
-                        success = True if confidence < 0.1 else False
-                else:
-                    if itr > 1000 or confidence > 0.8:
-                        finish = True
-                        success = True if confidence > 0.8 else False
+                        break
 
-                if verbose:
-                    print("Iteration: {} Confidence: {:.8}%".format(itr, confidence * 100))
-                    if finish:
-                        print("Image{} attack success: {}".format(idx, success))
-                        print("--------------------")  # end while
+                    if target is None:
+                        if target_class != predict_class:
+                            success = True
+                            break
+                    else:
+                        if target_class == predict_class:
+                            success = True
+                            break  # End inner for
+            # End outer for
 
             hacked_image = hacked_image[0]
             hacked_image *= 255
             hacked_image = hacked_image.astype('uint8')
-
             hacked_images.append(hacked_image)
+
+            print("Image{} attack success: {}".format(idx, success))
+            print("--------------------")
 
         return np.array(hacked_images)
--- main.py
+++ main.py
@@ -18,10 +18,10 @@ if __name__ == '__main__':
     (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
     x_train, x_test, y_train, y_test = np.expand_dims(x_train, -1), np.expand_dims(x_test, -1), np.expand_dims(y_train, -1), np.expand_dims(y_test, -1)
 
-    x = np.array(x_train[300:400])
-    y = y_train[300:400]
+    x = np.array(x_train[300:320])
+    y = y_train[300:320]
     samples = aiTest(x, x.shape)
-    np.save('gen/samples300_400.npy', samples)
+    np.save('gen/samples300_320.npy', samples)
     model = load_model('models/conv.h5')
     correct = model.evaluate(x/255, y)
-    fool = model.evaluate(np.load('gen/samples300_400.npy')/255, y)
+    fool = model.evaluate(np.load('gen/samples300_320.npy')/255, y)
